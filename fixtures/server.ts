9c2fdeedc8448d0372e8a97829c75d5a4f7b6a6c
johnche <john20chen@gmail.com>
2025-03-18 08:37:49 +0100

    update mock port, dynamic origin

--- fixtures/server.ts
+++ fixtures/server.ts
@@ -1,10 +1,10 @@
 const BASE_PATH = './fixtures';
-const PORT = 3000;
+const PORT = 5050;
 
 const server = Bun.serve({
 	port: PORT,
 	fetch(req: Request) {
-		const filePath = BASE_PATH + req.url.slice('http://localhost:3000'.length);
+		const filePath = BASE_PATH + new URL(req.url).pathname;
 		console.log(`Request: ${filePath}`);
 		const file = Bun.file(filePath);
 		return new Response(file);
